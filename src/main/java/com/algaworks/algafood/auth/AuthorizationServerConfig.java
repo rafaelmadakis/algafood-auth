c3cfcd8890ed7e6d67c6f36055d4f89415b4c910
rafaelmadakis <rafaelmadakis@gmail.com>
2020-04-10 00:00:22 -0300

    teste com client.js

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -37,6 +37,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
 
         .and()
+                .withClient("foodanalytics")
+                .secret(passwordEncoder.encode("food123"))
+                .authorizedGrantTypes("authorization_code")
+                .scopes("write", "read")
+                .redirectUris("http://www.foodanalytics.local:8082")
+
+        .and()
                  .withClient("faturamento")
                  .secret(passwordEncoder.encode("faturamento123"))
                  .authorizedGrantTypes("client_credentials")
