2c1d8440a98bf8aeb7ff9a619d38e436c2f33761
rafaelmadakis <rafaelmadakis@gmail.com>
2020-04-17 23:38:04 -0300

    Caixa Alta scopes

--- src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
@@ -50,7 +50,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient("algafood-web")
                 .secret(passwordEncoder.encode("web123"))
                 .authorizedGrantTypes("password", "refresh_token")
-                .scopes("write", "read")
+                .scopes("WRITE", "READ")
                 .accessTokenValiditySeconds(60 * 60 * 6) // 6 horas (padrão é 12 horas)
                 .refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
 
@@ -64,14 +64,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         .and()
                 .withClient("webadmin")
                 .authorizedGrantTypes("implicit")
-                .scopes("write", "read")
+                .scopes("WRITE", "READ")
                 .redirectUris("http://aplicacao-cliente ")
 
         .and()
                  .withClient("faturamento")
                  .secret(passwordEncoder.encode("faturamento123"))
                  .authorizedGrantTypes("client_credentials")
-                 .scopes("write", "read")
+                 .scopes("WRITE", "READ")
 
 
         .and()
